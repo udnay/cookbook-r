82caa95f2aec368b88dbf276fac57f6002dabb01
Nicholas Long <nicholas.long@nrel.gov>
2013-12-31 10:02:41 -0700

    add timout check to package library

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,8 +1,14 @@
-
 def package_installed?(package)
-  require "rinruby"
-  R.echo(enable=false)
-  R.eval "packages = installed.packages()[,1]"
-  packages = R.pull "packages"
-  return packages.include?(package)
+  require "timeout"
+  begin
+    Timeout::timeout(30) {
+      require "rinruby"
+      R.echo(enable=false)
+      R.eval "packages = installed.packages()[,1]"
+      packages = R.pull "packages"
+      return packages.include?(package)
+    }
+  rescue TimeoutError
+    raise "Timed out trying to check if package is installed using rinruby (is readline enabled in R?)"
+  end
 end
