c9e2082785e4e9b6a08a8194e46cb6073fbaffd2
Brian Ball <Brian.Ball@nrel.gov>
2014-02-28 15:01:50 -0700

    fixing nicks typos

--- libraries/default.rb
+++ libraries/default.rb
@@ -5,12 +5,25 @@ def package_installed?(package)
       require "rinruby"
       R.echo(enable=false)
       R.eval "local_packages <- installed.packages()[,c(1,3)]"
-      R.eval "local_version = local_packages['#{package}',2]"
-      R.eval "a <- available.packages()[,c(1,2)]"
-      R.eval "remote_version <- a['#{package}',2]"
-      R.eval "version_same <- (local_version == remote_version)"
-      versionSame = R.pull "version_same"
-      return versionSame
+      R.eval "package_in_list <- is.element('#{package}',local_packages)"
+      packageinlist = R.pull "as.character(package_in_list)"
+      if packageinlist == "TRUE"
+        R.eval "local_version = local_packages['#{package}',2]"
+        R.eval "a <- available.packages()[,c(1,2)]"
+        R.eval "package_in_list <- is.element('#{package}',a)"
+        packageinlist = R.pull "as.character(package_in_list)"
+        if packageinlist == "TRUE"
+          R.eval "remote_version <- a['#{package}',2]"
+          R.eval "version_same <- (local_version == remote_version)"
+          versionsame = R.pull "as.character(version_same)"
+          versionsame = versionsame == "TRUE"
+        else
+          versionsame = false
+        end  
+      else
+        versionsame = false
+      end
+      return versionsame
     }
   rescue TimeoutError
     raise "Timed out trying to check if package is installed using rinruby (is readline enabled in R?)"
