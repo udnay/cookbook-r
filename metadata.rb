e7da140c6fb7f0eaa8bde9992d47caddee53b134
Nicholas Long <nicholas.long@nrel.gov>
2014-04-03 08:15:03 -0600

    remove restriction on yum version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Installs/Configures R"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.3"
 
-depends "yum", "~> 2.4.4"
+depends "yum"
 depends "java"
 depends "apt"
 depends "ark"
